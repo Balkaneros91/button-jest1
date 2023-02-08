ed52bd4cf3d8ddba40c45708c9b7e4d4d0adec3b
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-02-08 13:25:37 +0000

    Add

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,34 +1,40 @@
-/**
- * @jest-environment jsdom
- */
-
 const buttonClick = require('../button');
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // document.body.innerHTML = "<p id='par'></p>";     // if it one component or many, but if you make changes in HTML you gotta make changes in the test as well !
+    let fs = require('fs');                             // a file system handling module built into Node that allows us to open read and write files !
+    let fileContent = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContent);
+    document.close();                         
 });
 
 describe('DOM tests', () => {
-    test('expects p content to change', () => {
+    test('Expects p content to change', () => {
         buttonClick();
-        expect(document.getElementById("par").innerHTML).toEqual('You Clicked');
+        expect(document.getElementById('par').innerHTML).toEqual('You Clicked');
     });
+    test('h1 should exist', () => {
+        expect(document.getElementsByTagName('h1').length).toBe(1);
+    }); 
 });
+
+// had to install this //   npm install --save-dev jest@26.6.3
 
 // /**
 //  * @jest-environment jsdom
 //  */
 
+// // const { beforeEach } = require('node:test');
 // const buttonClick = require("../button");
 
-// beforeAll(() => {
-//     document.body.innerHTML = "<p id='par'></p>";
+// beforeEach(() => {
+//     document.body.innerHTML = '<p id="par"></p>';
 // });
 
 // describe("DOM tests", () => {
-//     test("Expects content to change", () => {
+//     test("Expects p content to change", () => {
 //         buttonClick();
-//         expect(document.getElementById("par")
-//             .innerHTML).toEqual("You Clicked");
+//         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
 //     });
 // });
